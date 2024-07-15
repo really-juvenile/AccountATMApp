e04bf815bdebc5115f43816ba9207ca2f6d7b0e8
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-16 01:43:09 +0530

    Add project files.

---
+++ Models/Account.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp.Models
+{
+    internal class Account
+    {
+
+
+
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public string AdharNumber { get; set; }
+        static int MIN_BALANCE = 500;
+
+
+        public Account(int accountNo, string name)
+        {
+            AccountNumber = accountNo;
+            Name = name;
+            Balance = MIN_BALANCE;
+
+        }
+
+
+        public Account(int accountNumber, string name, double balance) : this(accountNumber, name)
+        {
+
+            if (balance < MIN_BALANCE)
+            {
+                Balance = MIN_BALANCE;
+
+            }
+            else
+            {
+                Balance = balance;
+            }
+        }
+
+        public Account(int accno, string name, double balance, string aadhar) : this(accno, name, balance)
+        {
+            {
+                AdharNumber = aadhar;
+            }
+
+        }
+
+        public bool Deposit(double amount)
+        {
+            Balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (Balance - amount < MIN_BALANCE)
+            {
+                Console.WriteLine("insufficient balance");
+                return false;
+            }
+            Balance -= amount;
+            return true;
+        }
+
+        public static Account AccountWithMaxBalance(Account[] accounts)
+
+        {
+
+            Account accountwithMaxBalance = null; //accounts[0];
+            double maxBalance = MIN_BALANCE;
+            foreach (Account account in accounts)
+            {
+
+                if (account.Balance > maxBalance)
+                {
+                    maxBalance = account.Balance;
+                    accountwithMaxBalance = account;
+
+                }
+
+            }
+            return accountwithMaxBalance;
+
+
+        }
+        public override string ToString()
+        {
+            return $"============Account No. {AccountNumber}\n" +
+                $"Account Holder name: {Name}\n" +
+                $"Account BAlance: {Balance}";
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using AccountApp.Models;
+
+namespace AccountApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //Account account1 = new Account(101, "Allen");
+            //Account account2 = new Account(102, "Mary", 5000);
+
+
+            Account[] accounts = new Account[] {
+                new Account(101,"Allen"),
+                new Account(102,"Mary")
+            };
+
+
+
+            //Deposit(account1, 1500);
+            //Withdraw(account1, 1700);
+            //Deposit(account2, 1000);
+            //Withdraw(account2, 1500);
+
+
+            DisplayMainMenu(accounts);
+
+            //Console.WriteLine(account1);
+            //Console.WriteLine(account2);
+
+
+            //Account maxAccount = Account.AccountWithMaxBalance(accounts);
+            //Console.WriteLine("\nAccount with max balance is : ");
+            //Console.WriteLine(maxAccount);
+
+        }
+
+        static void DisplayMainMenu(Account[] accounts)
+        {
+            Account accountInUse = null;
+            Console.WriteLine("============welcome to account management APP==============\n");
+            Console.WriteLine("Please Enter your account no. ");
+            int accountNo = Convert.ToInt32(Console.ReadLine());
+
+            foreach (Account account in accounts)
+            {
+
+                if (account.AccountNumber == accountNo)
+                {
+                    accountInUse = account;
+                }
+            }
+            Console.WriteLine($"\n Welcome {accountInUse.Name} \n");
+
+            while (true)
+            {
+
+                Console.WriteLine($"What do you wish to do? \n" +
+                    $"1. Deposit\n 2.Withdraw \n3.Check details \n4.Exit \nEnter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                DoTask(accountInUse, choice);
+
+            }
+
+        }
+
+        static void DoTask(Account accountInUse, int choice)
+        {
+            double amount;
+            switch (choice)
+            {
+                case 1:
+                    amount = GetAmount();
+                    Deposit(accountInUse, amount);
+                    break;
+
+                case 2:
+                    amount = GetAmount();
+                    Withdraw(accountInUse, amount);
+                    break;
+
+                case 3:
+                    Console.WriteLine(accountInUse);
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter valid Input");
+                    break;
+            }
+
+
+        }
+
+
+        static double GetAmount()
+        {
+            Console.WriteLine("Enter Amount: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        static void Deposit(Account account, double amount)
+        {
+            account.Deposit(amount);
+            Console.WriteLine($"amount of Rs. {amount} deposited in account {account.AccountNumber} successfully\n" +
+                $"Update Balance is: {account.Balance} ");
+
+            Console.WriteLine("==============================================");
+
+        }
+
+
+        static void Withdraw(Account account, double amount)
+        {
+            if (account.Withdraw(amount))
+            {
+                Console.WriteLine($"amount of Rs. {amount} withdraw from account {account.AccountNumber}\n" +
+                    $"Successfully \nupdated balance is: {account.Balance}");
+
+                Console.WriteLine("======================================");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Account Balance !! Withdrawl Denied");
+                Console.WriteLine("=============================================");
+            }
+
+        }
+
+    }
+}
